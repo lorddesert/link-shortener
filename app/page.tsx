dd489764acb2ce7c7c633e4a45d51e330914151e
lorddesert <sacha.nocetto@hotmail.com>
2024-04-02 21:24:48 -0300

    Extract copy button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { ReactElement, useState } from "react";
 import { Link } from "@/app/components/Link";
+import { CopyButton } from "@/app/components/CopyButton";
 const URL = `http://localhost:3000`
 
 export default function Home() {
@@ -33,7 +34,7 @@ export default function Home() {
             {shortLink.newURL}
           </Link>
         </hgroup>
-        <CopyButton shortLink={shortLink} />
+        <CopyButton URL={shortLink.newURL} />
       </div>
       }
     </main>
@@ -49,21 +50,3 @@ export default function Home() {
     };
   }
 }
-
-function CopyButton({ shortLink }: { shortLink: any }) {
-  const [copied, setCopied] = useState(false)
-  return <button onClick={() => {
-    navigator.clipboard.writeText(shortLink.newURL);
-    setCopied(true)
-
-    setTimeout(() => {
-      setCopied(false)
-    }, 2000)
-  }}>
-    {copied
-      ? <span>Copied✅</span>
-      : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="14" height="14" x="8" y="8" rx="2" ry="2" /><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" /></svg>
-    }
-  </button>;
-}
-
