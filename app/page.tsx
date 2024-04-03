654487fa4d9253043ac2b00ffb7baac7e9e08066
lorddesert <sacha.nocetto@hotmail.com>
2024-04-02 21:12:51 -0300

    Format, add intial state and more

--- app/page.tsx
+++ app/page.tsx
@@ -4,34 +4,52 @@ const URL = `http://localhost:3000`
 
 export default function Home() {
   const message = ""
-  const [shortLink, setShortLink] = useState('')
+  const [shortLink, setShortLink] = useState({
+    originalURL: '',
+    newURL: '',
+    alreadyExists: undefined
+  })
 
   return (
-    <main className="p-24 w-full flex-column justify-center">
-      <h1 className=" text-3xl mb-12">Link Shortener</h1>
-      <form onSubmit={async (e: any) => {
-        e.preventDefault()
-        const res = await (await fetch(`${URL}/shorten?link=${e.target[0].value}`)).json()
-        console.log(res)
-        setShortLink(res.newURL)
-      }}>
-        <label htmlFor="url" className="block mb-2">URL</label>
-        <input className="text-black p-2 rounded mr-2" type="url" name="link" id="link" pattern="https?://.*" placeholder="https://www.google.com" />
-
-        <button className="p-2 bg-white hover:bg-[#efefef] transition-colors text-black rounded" type="submit">Short link!</button>
-      </form>
-      {shortLink && <div className=" w-min-content flex gap-4 px-2 py-1 mr-3 text-black  bg-gray-300 rounded border mt-10">
-          <h3 className="text-2xl">{shortLink}</h3>
+    <main className="p-24">
+      <h1 className="text-3xl mb-12">Link Shortener</h1>
+      <div className="">
+        <form onSubmit={handleShortenLink}>
+          <label htmlFor="url" className="block mb-2">URL</label>
+          <input className="text-black p-2 rounded mr-2" type="url" name="link" id="link" pattern="https?://.*" placeholder="https://www.google.com" required />
+          <button className="p-2 bg-white hover:bg-[#efefef] transition-colors text-black rounded" type="submit">Short link!</button>
+        </form>
+      </div>
+      {shortLink.originalURL && <div className={`flex gap-4 px-2 py-1 mr-3 bg-slate-100 dark:bg-violet-950  rounded mt-10 border-l-8
+        ${shortLink.alreadyExists ? 'border-blue-600' : 'border-teal-700'}
+      `}>
+        <hgroup>
+          {shortLink.alreadyExists
+            ? <h2 className=" text-2xl">Link found!</h2>
+            : <h2 className=" text-2xl">Link created!</h2>
+          }
+          <a href={shortLink.newURL} className=" text-cyan-800 underline hover:text-cyan-900 dark:text-cyan-400 dark:hover:text-cyan-500 ">{shortLink.newURL}</a>
+        </hgroup>
         <CopyButton shortLink={shortLink} />
-        </div>}
+      </div>
+      }
     </main>
   );
+
+  function handleShortenLink(): string | ((formData: FormData) => void) | undefined {
+    return async (e: any) => {
+      e.preventDefault();
+      const res = await (await fetch(`${URL}/shorten?link=${e.target[0].value}`)).json();
+      setShortLink(res);
+
+      e.target.reset();
+    };
+  }
 }
-function CopyButton({ shortLink }) {
+function CopyButton({ shortLink }: { shortLink: any }) {
   const [copied, setCopied] = useState(false)
-  return <button className="bg-gray-400 p-1 rounded" onClick={() => {
-    navigator.clipboard.writeText(shortLink);
-    console.log(shortLink)
+  return <button onClick={() => {
+    navigator.clipboard.writeText(shortLink.newURL);
     setCopied(true)
 
     setTimeout(() => {
@@ -41,7 +59,7 @@ function CopyButton({ shortLink }) {
   }}>
     {copied
       ? <span>Copied✅</span>
-      : <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="14" height="14" x="8" y="8" rx="2" ry="2" /><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" /></svg>
+      : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="14" height="14" x="8" y="8" rx="2" ry="2" /><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" /></svg>
     }
   </button>;
 }
