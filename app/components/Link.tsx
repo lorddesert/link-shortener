613b2728734fa6501952aa9c1566bc6566712f8c
lorddesert <sacha.nocetto@hotmail.com>
2024-04-02 23:01:49 -0300

    Add target blank for anchor

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -4,7 +4,7 @@ export function Link({ href, children }: {
   href: string,
   children: ReactElement | string
 }) {
-  return <a href={href} className=" text-cyan-800 underline hover:text-cyan-900 dark:text-cyan-400 dark:hover:text-cyan-500 ">
+  return <a href={href} target="_blank" className=" text-cyan-800 underline hover:text-cyan-900 dark:text-cyan-400 dark:hover:text-cyan-500 ">
     {children}
   </a>;
 }
