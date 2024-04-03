73b2b286c4b84ece7a4a0aa15dce674254382413
lorddesert <sacha.nocetto@hotmail.com>
2024-04-02 21:13:28 -0300

    Add redirect

--- app/shorten/[link]/page.tsx
+++ app/shorten/[link]/page.tsx
@@ -1,30 +1,23 @@
-// import { redirect } from "next/navigation";
-
-import { readFile } from "fs/promises"
-import { cwd } from "node:process"
 import { getAllLinks } from "../route"
+import { redirect } from "next/navigation"
 
-// export async function GET(request: Request) {
-//   const params = new URL(request.url)
-//   console.log(params.pathname.)
-//   return Response.json({ status: 'OK' })
-//   // return redirect(originalLink)
-// }
 
 async function getShortLink(link: string) {
   const allLinks = await getAllLinks()
 
+  //@ts-ignore
   return Object.entries(allLinks).find(kv => kv[1].slice(-6) === link)
 }
 
 export default async function Page({ params }: { params: { link: string } }) {
-  const [originalLink, shortenLink] = await getShortLink(params.link)
-  return (
-    <div>
-      {/* {JSON.stringify(shortenLink)} */}
-      <h1>Holaaaa</h1>
-      <h2>Your shorten link is : <a className="hover:underline text-blue-400" href={originalLink}>{shortenLink}</a></h2>
-      <h3>Original Link: <a className="hover:underline text-blue-400" href={originalLink}>{ originalLink }</a></h3>
-    </div>
-  )
+  //@ts-ignore
+  const [originalLink] = await getShortLink(params.link)
+
+  redirect(originalLink)
+  // return (
+  //   <div>
+  //     <h2>Your shorten link is : <a className="hover:underline text-blue-400" href={originalLink}>{shortenLink}</a></h2>
+  //     <h3>Original Link: <a className="hover:underline text-blue-400" href={originalLink}>{ originalLink }</a></h3>
+  //   </div>
+  // )
 }
