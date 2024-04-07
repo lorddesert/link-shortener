e93ec812df8a75c9316b5ad2b757ebdd71f4757a
lorddesert <sacha.nocetto@hotmail.com>
2024-04-07 01:09:32 -0300

    use supabase for the api route

--- app/shorten/route.tsx
+++ app/shorten/route.tsx
@@ -1,54 +1,22 @@
 import { readFile, writeFile } from "fs/promises"
 import { cwd } from "process"
-
+import { initializeSupabaseClient } from "../lib/initializeSupabaseClient"
+import { ILink, generateShortKey, shortenLink } from "../lib/utils"
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const originalURL = searchParams.get('link') || ''
-  const allLinks = await getAllLinks()
-  const shortenLinkAlreadyExists = allLinks[originalURL]
+  const client = initializeSupabaseClient()
+  const originalURL = new URL(request.url).searchParams.get('link')!
 
-  if (shortenLinkAlreadyExists)
-    return Response.json({ originalURL, newURL: shortenLinkAlreadyExists, alreadyExists: true })
+  const { data }: {
+    data: any
+  } = await client.from('links').select('*').eq('originalURL', originalURL)
 
-  const newShortenLink = await shortenLink(originalURL, allLinks)
-  return Response.json({ originalURL, newURL: newShortenLink, alreadyExists: false })
-}
-
-
-async function generateShortKey() {
-  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-  const keyLength = 6 // Why 6?F
-
-  return Array.from({ length: keyLength }, () => {
-    const randomIndex = Math.floor(Math.random() * charset.length)
-    return charset[randomIndex]
-  })
-    .reduce((acum, curr) => acum + curr, "")
-}
-
-async function shortenLink(originalURL: string, allLinks: any) {
-  const newShortKey = await generateShortKey()
-  const newShortenLink = `http://localhost:3000/shorten/${newShortKey}`
-
-  await saveLinks(allLinks, originalURL, newShortenLink)
-  console.log(newShortenLink)
-  return newShortenLink
-}
-
-async function saveLinks(allLinks: any, originalURL: string, newShortenLink: string) {
-  const NEW_JSON_DATA = {
-    links: {
-      ...allLinks,
-      [originalURL]: newShortenLink
-    }
+  if (!data || !data?.length) {
+    console.log("OK 404: Data was falsy!")
+    const newShortKey = await shortenLink({ originalURL })
+    return Response.json({ originalURL, shortKey: `${newShortKey}`, alreadyExists: false })
   }
 
-  await writeFile(`${cwd()}/app/shorten/links.json`, JSON.stringify(NEW_JSON_DATA), { encoding: 'utf-8' })
-}
-
-export async function getAllLinks() {
-  const data = JSON.parse(
-    await readFile(`${cwd()}/app/shorten/links.json`, { encoding: 'utf-8' })
-  )
-  return data.links
+  console.log("OK 200: Link found!")
+  const { shortKey } = data[0]
+  return Response.json({ originalURL, shortKey: `${shortKey}`, alreadyExists: true })
 }
