0ec5bef575b6f29691dcd87922a914388207d4ac
lorddesert <sacha.nocetto@hotmail.com>
2024-04-23 21:52:58 -0300

    add mock dat

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -51,3 +51,76 @@ export async function shortenLink({ originalURL }: {
 
   return newShortKey
 }
+
+export const MOCK_ITEMS = [
+  {
+    "original_link": "https://www.goodreads.com/book/show/40961427-dune",
+    "author": "Frank Herbert",
+    "timestamp": "1609459200",
+    "id": "1",
+    "shortened_link": "https://goo.gl/1"
+  },
+  {
+    "original_link": "https://www.goodreads.com/book/show/11.The_Hitchhiker_s_Guide_to_the_Galaxy",
+    "author": "Douglas Adams",
+    "timestamp": "1609459200",
+    "id": "2",
+    "shortened_link": "https://goo.gl/2"
+  },
+  {
+    "original_link": "https://www.goodreads.com/book/show/3.Ender_s_Game",
+    "author": "Orson Scott Card",
+    "timestamp": "1609459200",
+    "id": "3",
+    "shortened_link": "https://goo.gl/3"
+  },
+  {
+    "original_link": "https://www.goodreads.com/book/show/13496.1984",
+    "author": "George Orwell",
+    "timestamp": "1609459200",
+    "id": "4",
+    "shortened_link": "https://goo.gl/4"
+  },
+  {
+    "original_link": "https://www.goodreads.com/book/show/242233.Rendezvous_with_Rama",
+    "author": "Arthur C. Clarke",
+    "timestamp": "1609459200",
+    "id": "5",
+    "shortened_link": "https://goo.gl/5"
+  },
+  {
+    "original_link": "https://www.goodreads.com/book/show/51496.Foundation",
+    "author": "Isaac Asimov",
+    "timestamp": "1609459200",
+    "id": "6",
+    "shortened_link": "https://goo.gl/6"
+  },
+  {
+    "original_link": "https://www.goodreads.com/book/show/77566.Neuromancer",
+    "author": "William Gibson",
+    "timestamp": "1609459200",
+    "id": "7",
+    "shortened_link": "https://goo.gl/7"
+  },
+  {
+    "original_link": "https://www.goodreads.com/book/show/18490.Brave_New_World",
+    "author": "Aldous Huxley",
+    "timestamp": "1609459200",
+    "id": "8",
+    "shortened_link": "https://goo.gl/8"
+  },
+  {
+    "original_link": "https://www.goodreads.com/book/show/9989.Snow_Crash",
+    "author": "Neal Stephenson",
+    "timestamp": "1609459200",
+    "id": "9",
+    "shortened_link": "https://goo.gl/9"
+  },
+  {
+    "original_link": "https://www.goodreads.com/book/show/24280.Dune_Messiah",
+    "author": "Frank Herbert",
+    "timestamp": "1609459200",
+    "id": "10",
+    "shortened_link": "https://goo.gl/10"
+  }
+]
