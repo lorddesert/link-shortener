643d246c1872c4aa9337a3ce1d038d2bb6831641
lorddesert <sacha.nocetto@hotmail.com>
2024-04-23 21:52:05 -0300

    add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontFamily: {
+      'poppins-regular': ['Poppins'],
+      'poppins-medium': ['Poppins-Medium'],
+      'poppins-bold': ['Poppins-Bold'],
+    }
   },
   plugins: [],
 };
