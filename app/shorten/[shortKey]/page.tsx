5b1f94462204a83d5195ef8953afd43d0a678c49
lorddesert <sacha.nocetto@hotmail.com>
2024-04-23 21:53:04 -0300

    remove console log

--- app/shorten/[shortKey]/page.tsx
+++ app/shorten/[shortKey]/page.tsx
@@ -6,7 +6,6 @@ async function getShortLink(shortKey: string) {
   const client = initializeSupabaseClient()
 
   const { data } = await client.from('links').select("*").eq("shortKey", shortKey)
-  console.log(data)
   if (!data) return "/404"
 
   return data[0].originalURL
@@ -16,6 +15,5 @@ export default async function Page({ params }: { params: { shortKey: string } })
   //@ts-ignore
   const originalURL = await getShortLink(params.shortKey)
 
-  console.log(originalURL)
   redirect(originalURL)
 }
