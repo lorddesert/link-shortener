8b3d0d2a2914592bffd96858212d20b0223ab839
lorddesert <sacha.nocetto@hotmail.com>
2024-04-27 09:01:30 -0300

    Change naming

--- app/shorten/[shortKey]/page.tsx
+++ app/shorten/[shortKey]/page.tsx
@@ -2,18 +2,17 @@ import { initializeSupabaseClient } from "@/app/lib/initializeSupabaseClient"
 import { redirect } from "next/navigation"
 
 
-async function getShortLink(shortKey: string) {
+async function getOriginalURL(shortKey: string) {
   const client = initializeSupabaseClient()
 
-  const { data } = await client.from('links').select("*").eq("shortKey", shortKey)
+  const { data } = await client.from('links').update({ lastTimeUsed: new Date() } ).eq('shortKey', shortKey).select()
   if (!data) return "/404"
 
   return data[0].originalURL
 }
 
 export default async function Page({ params }: { params: { shortKey: string } }) {
-  //@ts-ignore
-  const originalURL = await getShortLink(params.shortKey)
+  const originalURL = await getOriginalURL(params.shortKey)
 
   redirect(originalURL)
 }
