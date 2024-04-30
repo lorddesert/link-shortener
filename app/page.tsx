0a49dc93665b1e6d30e259e1aabcad2e2b4b4c97
lorddesert <sacha.nocetto@hotmail.com>
2024-04-30 18:01:46 -0300

    Move static content to layout

--- app/page.tsx
+++ app/page.tsx
@@ -10,26 +10,16 @@ export default async function Home() {
   const lastTenLinksCreated = await response.json()
 
   return (
-    <main className="p-10 md:p-20 max-w-4xl mx-auto font-poppins-regular">
-      <Toaster position="top-center" />
-      <div className="flex justify-center">
-        {/* <h1 className="text-6xl mb-12 text-center font-bold bg-gradient-to-r from-[var(--highlight)] to-[var(--highlight2)] text-transparent bg-clip-text inline-block mx-auto">L<span className="opacity-40">inq</span>RL</h1> */}
-        <h1 className="text-5xl mb-32 font-bold">Linq</h1>
-      </div>
-      <h2 className="text-2xl text-balance text-center font-semi-bold mb-4">The Simplest URL Shortener You Were Wainting For</h2>
-      <ShortenLinkForm />
-      {/* {shortLink.originalURL && NewLinkCard(shortLink)} */}
-      <section className="last-ten-links-created mt-4">
-        {/* <LastTenLinksCreated /> */}
-        <IsBeingDevWrapper>
-          <ul className="flex flex-wrap gap-4 ">
-            {lastTenLinksCreated.data.map((link: ILinkItem) => (
-              <LinkItem link={link} key={`${link.id}`} />
-            )
-            )}
-          </ul>
-        </IsBeingDevWrapper>
-      </section>
-    </main>
+    <div className="last-ten-links-created mt-4">
+      {/* <LastTenLinksCreated /> */}
+      <IsBeingDevWrapper>
+        <ul className="flex flex-wrap gap-4 ">
+          {lastTenLinksCreated.data.map((link: ILinkItem) => (
+            <LinkItem link={link} key={`${link.id}`} />
+          )
+          )}
+        </ul>
+      </IsBeingDevWrapper>
+    </div>
   );
 }
