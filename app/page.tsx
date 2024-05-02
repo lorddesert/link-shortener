de22a9ddbd092735d1a1f9c9b20691cc5795a6b9
lorddesert <sacha.nocetto@hotmail.com>
2024-05-01 22:06:13 -0300

    Remove content from layout and move to page
    This ensures that we can re-use the layout without render the short link form on the error page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,35 @@ import { ShortenLinkForm } from "./components/ShortenLinkForm";
 import { Toaster } from "sonner";
 import { IsBeingDevWrapper } from "./components/IsBeingDevWrapper";
 import { LinkItem } from "./components/LinkItem";
-import { ILinkItem } from "./lib/utils";
+import { ILinkItem, MOCK_ITEMS } from "./lib/utils";
 
 export default async function Home() {
   const response = await fetch(`http://localhost:3000/api/links/get/last-ten`, { cache: 'no-store' })
+  if (!response.ok) {
+    //TODO: handle error 500
+  }
+  console.log('Home response:', response)
   const lastTenLinksCreated = await response.json()
+  // const lastTenLinksCreated = response
 
   return (
-    <div className="last-ten-links-created mt-4">
-      {/* <LastTenLinksCreated /> */}
-      <IsBeingDevWrapper>
-        <ul className="flex flex-wrap gap-4 ">
-          {lastTenLinksCreated.data.map((link: ILinkItem) => (
-            <LinkItem link={link} key={`${link.id}`} />
-          )
-          )}
-        </ul>
-      </IsBeingDevWrapper>
-    </div>
+    <>
+      <Toaster position="top-center" />
+      <div className="flex justify-center">
+        <h1 className="text-5xl mb-32 font-bold">Linq</h1>
+      </div>
+      <h2 className="text-2xl text-balance text-center font-semi-bold mb-4">The Simplest URL Shortener You Were Wainting For</h2>
+      <ShortenLinkForm />
+      <div className="last-ten-links-created mt-4">
+        <IsBeingDevWrapper>
+          <ul className="grid grid-cols-1 place-items-center md:grid-cols-2 gap-4 ">
+            {MOCK_ITEMS.map((link: any) => (
+              <LinkItem link={link} key={`${link.id}`} />
+            )
+            )}
+          </ul>
+        </IsBeingDevWrapper>
+      </div>
+    </>
   );
 }
