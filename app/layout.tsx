b2139676c51734958bd541db2d20f703f0b6e5c2
lorddesert <sacha.nocetto@hotmail.com>
2024-05-02 22:41:58 -0300

    Update layout, move Toaster here

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { ShortenLinkForm } from "./components/ShortenLinkForm";
+import { Button } from "./components/Button";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +21,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Toaster position="top-center" />
+        <header className="p-2 flex justify-between sticky top-0 z-50 backdrop-filter backdrop-blur-sm">
+          <Link href="/">
+            <h1 className="text-xl">
+              lord<span className="text-cyan-500 underline">links</span>
+            </h1>
+          </Link>
+          {/* <Button variant="ghost">Login</Button> */}
+        </header>
         <main className="p-10 md:p-20 max-w-4xl mx-auto font-poppins-regular">
           {children}
         </main>
