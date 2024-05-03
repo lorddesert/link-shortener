41cdf7a946fd43218d09d31ad735b8b7cae3cb5b
lorddesert <sacha.nocetto@hotmail.com>
2024-05-03 00:50:01 -0300

    Delete

--- app/lib/initializeSupabaseClient.ts
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-export function initializeSupabaseClient() {
-  return createClient(process.env.SUPABASE_PROJECT_URL!, process.env.SUPABASE_API_KEY!)
-}
