5cf567dece88b967e910ebc3cf73a192eed4f08a
lorddesert <sacha.nocetto@hotmail.com>
2024-05-03 18:13:04 -0300

    Add disabled class

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -19,7 +19,7 @@ export function Button({
   ${variant === 'default' && `p-2 border-gray-500 border bg-zinc-900 rounded-lg h-full outline-none focus:border-gray-200`} 
   ${variant === 'ghost' && `p-2 border-gray-500 border bg-zinc-900 rounded-lg outline-none border-1 border-transparent transition-colors hover:bg-zinc-700 focus:border-zinc-600 h-auto bg-opacity-0 active:bg-zinc-600`}
   ${variant === 'danger' && `p-2 border-red-700 border bg-zinc-900 rounded-lg outline-none border-1 border-transparent transition-colors hover:bg-red-700 focus:border-red-600 h-auto bg-opacity-0 active:bg-red-600`}
-
+  ${disabled && 'hover:cursor-not-allowed opacity-60'}
   ${cn}
   `}>
     {children}
