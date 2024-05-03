b75516edc2f24de4629e42d181748cccf2b7b89b
lorddesert <sacha.nocetto@hotmail.com>
2024-05-03 00:49:39 -0300

    Add poppins font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { ShortenLinkForm } from "./components/ShortenLinkForm";
-import { Button } from "./components/Button";
-import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight: ['600', '400'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,17 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Toaster position="top-center" />
-        <header className="p-2 flex justify-between sticky top-0 z-50 backdrop-filter backdrop-blur-sm">
-          <Link href="/">
-            <h1 className="text-xl">
-              lord<span className="text-cyan-500 underline">links</span>
+        <main className="p-10 md:p-20 max-w-4xl mx-auto">
+          <header className="flex justify-center">
+            <h1 className="flex justify-centertext-center mb-10 text-3xl relative">
+              <span>linq</span>
             </h1>
-          </Link>
-          {/* <Button variant="ghost">Login</Button> */}
-        </header>
-        <main className="p-10 md:p-20 max-w-4xl mx-auto font-poppins-regular">
+          </header>
           {children}
         </main>
       </body>
