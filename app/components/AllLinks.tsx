85d2d68a668a128f20deccbfc1b5c901f7a22c2e
lorddesert <sacha.nocetto@hotmail.com>
2024-05-03 00:49:50 -0300

    Remove dev wrapper

--- app/components/AllLinks.tsx
+++ app/components/AllLinks.tsx
@@ -1,20 +1,18 @@
+import { getAllLinks } from "../actions"
 import { ILinkItem } from "../lib/utils"
-import { IsBeingDevWrapper } from "./IsBeingDevWrapper"
 import { LinkItem } from "./LinkItem"
 export async function AllLinks() {
-  const response = await fetch(`http://localhost:3000/api/links/get/last-ten`, { cache: 'no-store'})
-  const allLinks = await response.json()
+  const allLinks = await getAllLinks()  
+  
   return (
-    <IsBeingDevWrapper>
-      <div className="mt-4">
-        <ul className="grid grid-cols-1 place-items-center md:grid-cols-2 gap-3 ">
-          {allLinks.data.map((link: ILinkItem) => (
-            <LinkItem link={link} key={`${link.id}`} />
-          )
-          )}
-        </ul>
-      </div>
-    </IsBeingDevWrapper>
+    <div className="mt-4">
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-3 ">
+        {allLinks.data.map((link: ILinkItem) => (
+          <LinkItem link={link} key={`${link.id}`} />
+        )
+        )}
+      </ul>
+    </div>
 
   )
 }
