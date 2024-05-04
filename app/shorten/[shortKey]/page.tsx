72bf2e7abf78cd8c74064c33460205745395a494
lorddesert <sacha.nocetto@hotmail.com>
2024-05-03 21:37:07 -0300

    Add extra validation for error

--- app/shorten/[shortKey]/page.tsx
+++ app/shorten/[shortKey]/page.tsx
@@ -5,8 +5,8 @@ import { redirect } from "next/navigation"
 async function getOriginalURL(shortKey: string) {
   const client = await initializeSupabaseClient()
 
-  const { data } = await client.from('links').update({ lastTimeUsed: new Date() } ).eq('shortKey', shortKey).select()
-  if (!data) return "/404"
+  const { data, error } = await client.from('links').update({ lastTimeUsed: new Date() } ).eq('shortKey', shortKey).select()
+  if (error || !data.length) return "/404"
 
   return data[0].originalURL
 }
