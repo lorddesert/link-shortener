b2a300dd58677a25c942c9aaf61abf232a6d1c16
lorddesert <sacha.nocetto@hotmail.com>
2024-05-28 22:29:15 -0300

    Add hover effect

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -15,7 +15,8 @@ export function Button({
   disabled?: boolean,
   onClick?: (e?: any) => void
 }) {
-  return <button disabled={disabled} type={type || "button"} {...restOfProps} className={` 
+  return <button disabled={disabled} type={type || "button"} {...restOfProps} className={`
+  hover:border-gray-200
   ${variant === 'default' && `p-2 border-gray-500 border bg-zinc-900 rounded-lg h-full outline-none focus:border-gray-200`} 
   ${variant === 'ghost' && `p-2 border-gray-500 border bg-zinc-900 rounded-lg outline-none border-1 border-transparent transition-colors hover:bg-zinc-700 focus:border-zinc-600 h-auto bg-opacity-0 active:bg-zinc-600`}
   ${variant === 'danger' && `p-2 border-red-700 border bg-zinc-900 rounded-lg outline-none border-1 border-transparent transition-colors hover:bg-red-700 focus:border-red-600 h-auto bg-opacity-0 active:bg-red-600`}
